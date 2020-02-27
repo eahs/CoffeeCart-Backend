4c391616c118c77fe72d6081b3baee772738c3b6
Elijah Horn <555elihorn@gmail.com>
2020-02-27 10:56:37 -0500

    New Product Model

---
+++ ADSBackend/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ADSBackend.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public Type Type { get; set; }
+        public List<Option> Options { get; set; }
+    }
+
+    public enum Type
+    {
+        Regular, Tea, Express
+    }
+}
