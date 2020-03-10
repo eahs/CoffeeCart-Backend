bff305c0a2263e26ca2819a07237ecdea6a0de01
Elijah Horn <555elihorn@gmail.com>
2020-03-10 10:40:30 -0400

    Scaffolding for newer Product

--- ADSBackend/Models/Product.cs
+++ ADSBackend/Models/Product.cs
@@ -8,13 +8,13 @@ namespace ADSBackend.Models
 {
     public class Product
     {
-        int Id { get; set; }
+        public int Id { get; set; }
         [Required]
         [Display(Name = "Name")]
-        string Name { get; set; }
+        public string Name { get; set; }
         [Display(Name = "Options")]
-        string Options { get; set; }
+        public string Options { get; set; }
         [Display(Name = "Addons")]
-        string Addons { get; set; }
+        public string Addons { get; set; }
     }
 }
